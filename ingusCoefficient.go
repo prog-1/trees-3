d25b7d53bd4e6b0660c6ff13a123e8e6d1b98083
vay4ie <75880193+vay4ie@users.noreply.github.com>
2022-12-07 23:51:23 +0200

    Rewrote min IC function

--- ingusCoefficient.go
+++ ingusCoefficient.go
@@ -2,110 +2,93 @@ package main
 
 import "fmt"
 
-//iK - Ingus koeficient
-
-//n - total task amount
-//m - task amount that Ingus check every day
-
-// Ingus is taking m task in day
-// Ingus is trying to solve then in any order
-// With every solved task, IK raises on 1
-// If some task is too hard, he keeps it in next day
-// if we can't solve all tasks of the day, we end
-
-//Result:
-//which starter IK should be to solve all tasks
-//How many days is needed to solve all tasks with selected starter IK
-
-//Input
-// N M  // N - total task amount  M - tasks per day
-// 3 1 7 2 6 3 // each task difficulty level
-
-//Output
-// sIK D
-// sIK - starter IK | D - day count
-
+type node struct {
+	val, count  int
+	left, right *node
+}
 
 func main() {
-	//Works at least somehow
-	book := []int{3, 1, 7, 2, 6, 3}
-	TperD := 3
-	//Don't work at all
-	// book := []int{7, 6, 5, 4, 3}
-	// TperD := 9
-	fmt.Println(Ingus(TperD, book))
+	dta := 9
+	book := []int{28, 59, 87, 19, 19, 17, 69, 72}
+	fmt.Println(ingusCoefficient(book, dta))
 }
 
-//minTI - min Task Level Index
-//tA - task Amount
-
-func Ingus(TperD int, book []int) (int, int) {
-
-	//Variable declaration
-	//var solveMinTask func() //pre declaration to use before implementation
-	//var nextDay func()
-	var days int           // days count
-	if TperD > len(book) { //if task per day amount is bigger that task amount in book
-		TperD = len(book) //make it equal to book task amount
+func ingusCoefficient(book []int, dta int) int {
+	var ic, mic, nt, st int
+	var t *node
+	var min *node
+	if dta > len(book) {
+		dta = len(book)
 	}
-	dayTasks := book[:TperD] //taking first TperD elements
-	//var dayTasks []int
-	book = book[TperD:] //deleting first TperD elements from book
-	//minTI := takeMin(dayTasks) //taking min task index (index to be able to delete it)
-	var minTI int
 
-	startIK := dayTasks[takeMin(dayTasks)] // making that iK is min task lvl, because it's necessary starting point
-	//iK := startIK
-	//dayTasks = append(dayTasks[:minTI], dayTasks[minTI+1:]...) //delete 1st min task (mb swap with 1st and delete 1st element?)
-
-	for len(dayTasks) != 0 {
-		b := book
-		iK := startIK
-		days = 0
-		for len(b) != 0 { //while we haven't found the ik which will solve all tasks
-
-			//next day
-			tA := TperD - len(dayTasks) //calculate how much tasks we solved and need to take
-			if tA > len(b) {            //if we want to take more task then is in book
-				tA = len(b) //take the amount of task that left in book
+	for len(book) != 0 || t.len() != 0 {
+		if min != nil && ic >= min.val {
+			delMin(&t)
+			ic++
+			st++
+			min = t.min()
+		} else if t.len() < dta && len(book) != 0 {
+			nt = dta - t.len()
+			for i := 0; i < nt; i++ {
+				t = t.add(book[i])
 			}
-			dayTasks = append(dayTasks, b[:tA]...) //add new tasks in day tasks
-			b = b[tA:]                             //delete taken tasks from book
-
-			for range dayTasks {
-				minTI = takeMin(dayTasks)
-				if dayTasks[minTI] <= iK { // if we can solve task
-					dayTasks = append(dayTasks[:minTI], dayTasks[minTI+1:]...) //delete this task (mb swap with 1st and delete 1st element?)
-					iK++                                                       // increase iK
-				} else {
-					break //else we can't solve any more tasks today
-				}
-			}
-
-			// //if we don't have any changes since previous day
-			// if prevDay == len(dayTasks){
-			// 	break
-			// }
-			// prevDay = len(dayTasks)//updating day progress
-			days++ //increasing day count
+			book = book[nt:]
+			min = t.min()
+		} else /*dt.len() == dta*/ {
+			mic = min.val - st
+			ic = mic + st
 		}
-		startIK++ //try one more time with koefficient +1
-		dayTasks = nil
 	}
-	return startIK, days
-
-	//we need to repeat solving many times
-	//how to act when we stopped? When nothing is changing day after day
+	return mic
 
 }
 
-//return index
-func takeMin(s []int) int {
-	res := 0
-	for i := range s {
-		if s[i] < s[res] {
-			res = i
+/*
+dta - day task amount
+t - day task tree
+nt - new tasks
+st - solved tasks
+*/
+
+func (t *node) add(val int) *node {
+	n := &t
+	for *n != nil {
+		if val < (*n).val {
+			n = &(*n).left
+		} else {
+			n = &(*n).right
 		}
 	}
-	return res
+	*n = &node{val: val}
+	return t
+}
+
+func delMin(t **node) {
+	for (*t).left != nil {
+		t = &(*t).left
+	}
+	*t = (*t).right
+}
+
+func (t *node) min() *node {
+	if t == nil {
+		return t
+	}
+	for ; t.left != nil; t = t.left {
+	}
+	return t
+}
+
+func (t *node) len() int {
+	var l int
+	if t == nil {
+		return l
+	} else if t.count == 0 {
+		l++
+	} else {
+		for i := 0; i < t.count; i++ {
+			l++
+		}
+	}
+	return l + t.left.len() + t.right.len()
 }
