8079c5ce186b6c760a0614f02dd8bb0c4fb99492
vay4ie <75880193+vay4ie@users.noreply.github.com>
2022-12-08 00:29:59 +0200

    Added day count and input processing

--- ingusCoefficient.go
+++ ingusCoefficient.go
@@ -8,18 +8,29 @@ type node struct {
 }
 
 func main() {
-	dta := 9
-	book := []int{28, 59, 87, 19, 19, 17, 69, 72}
+
+	var t, dta int
+	fmt.Scanf("%v%v", &t, &dta)
+	book := make([]int, t)
+	for i := 0; i < t; i++ {
+		fmt.Scan(&book[i])
+	}
+
+	//dta := 4
+	//book := []int{28, 59, 87, 19, 19, 17, 69, 72}
+	//dta := 2
+	//book := []int{5, 3, 1, 6, 3, 5, 3}
 	fmt.Println(ingusCoefficient(book, dta))
 }
 
-func ingusCoefficient(book []int, dta int) int {
-	var ic, mic, nt, st int
+func ingusCoefficient(book []int, dta int) (int, int) {
+	var ic, mic, nt, st, days int
 	var t *node
 	var min *node
 	if dta > len(book) {
 		dta = len(book)
 	}
+	book2 := book //for day count
 
 	for len(book) != 0 || t.len() != 0 {
 		if min != nil && ic >= min.val {
@@ -29,7 +40,7 @@ func ingusCoefficient(book []int, dta int) int {
 			min = t.min()
 		} else if t.len() < dta && len(book) != 0 {
 			nt = dta - t.len()
-			for i := 0; i < nt; i++ {
+			for i := 0; i < nt && i < len(book); i++ {
 				t = t.add(book[i])
 			}
 			book = book[nt:]
@@ -39,7 +50,29 @@ func ingusCoefficient(book []int, dta int) int {
 			ic = mic + st
 		}
 	}
-	return mic
+
+	ic = mic
+	nt = 0
+	//Day Count
+	for len(book2) != 0 || t.len() != 0 {
+		if min != nil && ic >= min.val {
+			delMin(&t)
+			ic++
+			min = t.min()
+		} else if t.len() < dta && len(book2) != 0 {
+			nt = dta - t.len()
+			for i := 0; i < nt && i < len(book2); i++ {
+				t = t.add(book2[i])
+				if i < len(book2) {
+					book2 = book2[1:]
+				}
+			}
+			min = t.min()
+			days++
+		}
+	}
+
+	return mic, days - 1
 
 }
 
